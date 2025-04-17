24664ae71d0a52f4831553db5799d518a52c3f31
Tax Simples Tech <tecnologia@office-ce.com.br>
2025-04-17 18:03:02 -0300

    Criado o leitor de planilhas google

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from services.ipValidator import get_public_ip
 from services.downloadPFX import download_pfx
 from services.installPFX import install_pfx
+from services.sheetReader import read_google_sheets
 from dotenv import load_dotenv
 import os
 
@@ -12,8 +13,10 @@ FIXED_IP = os.getenv("FIXED_IP")
 if FIXED_IP == get_public_ip():
 
     url = "https://drive.google.com/file/d/1p8_nr7K0vsiuWqLXkNYinLwuGl4EWhoF/view?usp=sharing"
-    download_pfx(url)
-    install_pfx("123456")
-
+    urlSheets = "https://docs.google.com/spreadsheets/d/1uaQCpft_pyLkOYkpT48ZQgoMvXwBwlXcn9AKxDMBgH8/edit?usp=sharing"
+    # download_pfx(url)
+    # install_pfx("123456")
+    df = read_google_sheets(urlSheets)
+    print(df)
 else:
     print("Você não está na sua organização, falar com TI!")
